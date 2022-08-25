55507b8a4c24f2da0d467f2a8fd82688a1001b59
Евгений <ivanychcos@gmail.com>
2022-08-25 14:10:57 +0300

    add method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+int [] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int x = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < x)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
